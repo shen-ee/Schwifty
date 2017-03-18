eabc50c792ae1ff90a1693b9f8b4ff4cbd26dc0d
Shen Yi <jerry31@qq.com>
2017-03-18 17:29:10 +0800

    empty project

---
+++ Schwifty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Schwifty
+//
+//  Created by Shen Yi on 2017/3/18.
+//  Copyright © 2017年 Shen Yi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
